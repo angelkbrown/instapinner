0a9cc3c8aa57fb91472607448c50f6897dcdece4
angel brown <angel.k.brown@gmail.com>
2011-10-24 22:19:24 -0400

    adding Instagram config stuff back; removing extra views

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require "instagram"
 require "haml"
 enable :sessions
 
+Instagram.configure do |config|
+  config.client_id = "608edd598bd24939a5ea469cb9c2e34c"
+  config.client_secret = "757673aa3bcb45a5a7e1bbb3ff693a3b"
+end
+
 get "/" do
 	haml :search
 end
